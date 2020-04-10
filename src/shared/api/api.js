95ecf2dcff859e34e092fc7565aa5e03794af88a
Diego Fontenelle <diego.lima.fontenelle@gmail.com>
2020-04-10 17:35:14 -0300

    chore(api): add interceptors for response duration

--- src/shared/api/api.js
+++ src/shared/api/api.js
@@ -5,4 +5,25 @@ const api = axios.create({
   timeout: 8000,
 })
 
+api.interceptors.request.use(config => {
+  const reqConfig = config
+  reqConfig.metadata = { startTime: new Date() }
+
+  return reqConfig
+}, error => Promise.reject(error))
+
+api.interceptors.response.use(response => {
+  const res = response
+  res.config.metadata.endTime = new Date()
+  res.duration = (res.config.metadata.endTime - response.config.metadata.startTime) / 1000
+
+  return res
+}, error => {
+  const err = error
+  err.config.metadata.endTime = new Date()
+  err.duration = (err.config.metadata.endTime - error.config.metadata.startTime) / 1000
+
+  return Promise.reject(err)
+})
+
 export default api
