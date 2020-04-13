aa9a68a988750a54f95b4b84c18c5617570f3cf7
Diego Fontenelle <diego.lima.fontenelle@gmail.com>
2020-04-13 05:47:14 -0300

    chore(jest): add coverage ignore paths

--- jest.config.js
+++ jest.config.js
@@ -5,12 +5,22 @@ module.exports = {
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['src/**/*js', '!src/**/*.style.js', '!src/index.js'],
+  collectCoverageFrom: [
+    'src/**/*js',
+    '!src/**/index.js',
+    '!src/**/*.style.js',
+    '!src/index.js',
+    '!src/shared/**/*.js',
+    '!src/store/*',
+    '!src/reducers/*',
+    '!src/actions/*',
+    '!src/routes/*',
+  ],
   setupFilesAfterEnv: ['./setupTests.js'],
   moduleNameMapper: {
-    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js', '\\.(svg)$': '<rootDir>/__mocks__/svgrMock.js',
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(svg)$': '<rootDir>/__mocks__/svgrMock.js',
   },
-  testPathIgnorePatterns: [
-    '<rootDir>/__mocks__/',
-  ],
+  testPathIgnorePatterns: ['<rootDir>/__mocks__/'],
 }
