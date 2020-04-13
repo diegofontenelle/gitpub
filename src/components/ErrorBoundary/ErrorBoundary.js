fcb06dc788af6d2787ee62454ac2d7f63ce57a6a
Diego Fontenelle <diego.lima.fontenelle@gmail.com>
2020-04-13 06:44:51 -0300

    feature: add error boundary

---
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { element, func, node, oneOfType } from 'prop-types'
+import { Container, ErrorMessage, Message, Title } from './ErrorBoundary.style'
+
+export default class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { error: null, errorInfo: null }
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      error,
+      errorInfo,
+    })
+  }
+
+  render() {
+    const { error, errorInfo } = this.state
+    const { children } = this.props
+    if (errorInfo) {
+      return (
+        <Container>
+          <Title>Oops, something went wrong.</Title>
+          <Message>
+            This is probably a problem in our end, sorry about that :(
+          </Message>
+          <ErrorMessage>{error && error.toString()}</ErrorMessage>
+        </Container>
+      )
+    }
+
+    return children
+  }
+}
+
+ErrorBoundary.propTypes = {
+  children: oneOfType([element, node, func]).isRequired,
+}
---
+++ src/components/ErrorBoundary/ErrorBoundary.style.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  background-image: Bg;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+  position: absolute;
+  width: 100%;
+`
+
+const Message = styled.p`
+  color: ${props => props.theme.colors.text.main};
+  font-size: ${props => props.theme.fontSizes.small};
+`
+
+const ErrorMessage = styled(Message)`
+  color: ${props => props.theme.colors.error};
+`
+
+const Title = styled.h1`
+  color: ${props => props.theme.colors.text.main};
+  font-size: ${props => props.theme.fontSizes.large};
+`
+
+export { Container, ErrorMessage, Message, Title }
