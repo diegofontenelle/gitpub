ba20234d728d586445afd64f8c45b5d35a01b4be
Diego Fontenelle <diego.lima.fontenelle@gmail.com>
2020-04-21 19:26:37 -0300

    refactor(EmptyState): change how messages are set

--- src/components/EmptyState/EmptyState.js
+++ src/components/EmptyState/EmptyState.js
@@ -4,12 +4,13 @@ import { Container, Message } from './EmptyState.style'
 import { Background, Computer } from '../../shared/animations'
 
 const EmptyState = ({ didFetch }) => {
-  const mainMessage = didFetch
-    ? 'Roses are red, violets are blue'
-    : 'Wow such empty'
-  const message = didFetch
-    ? 'We did not find anything for you'
-    : 'Start typing and let us do the rest'
+  let mainMessage = 'Wow such empty'
+  let message = 'Start typing and let us do the rest'
+
+  if (didFetch) {
+    mainMessage = 'Roses are red, violets are blue'
+    message = 'We did not find anything for you'
+  }
 
   return (
     <Container>
